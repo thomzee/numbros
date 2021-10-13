027aedca5ef1fd9d41ea4070663a49d3c76c6cf1
Thomas Andrianto <thomaskayu@gmail.com>
2021-10-13 14:42:44 +0700

    Revert "Create .gitignore file"

--- src/Factory/NumbrosNumbers.php
+++ src/Factory/NumbrosNumbers.php
@@ -5,11 +5,6 @@ namespace Thomzee\Numbros\Factory;
 
 class NumbrosNumbers implements NumbrosInterface
 {
-    /**
-     * This function return numbers of date with separators
-     * @param $separator
-     * @return string
-     */
     public function generate($separator)
     {
         $now = date('Ymd');
